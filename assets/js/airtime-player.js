2392ca07a27c2ad5010feb1a713fda137d8af420
Kanwar Ammar <59098958+kanwar-ammar@users.noreply.github.com>
2026-06-03 13:18:32 +0100

    Remove port 8000 from stream URLs — HTTPS proxy uses port 443

--- assets/js/airtime-player.js
+++ assets/js/airtime-player.js
@@ -47,12 +47,12 @@ var MusesPlayer = function() {
         this.availableMobileStreamQueue = [];
         this.availableDesktopStreamQueue = [
         {
-            "url":"https:\/\/stream.radioangrezi.de:8000\/live",
+            "url":"https:\/\/stream.radioangrezi.de\/live",
             "codec":"mp3",
             "bitrate":192,
             "mobile":"0"
         },{
-            "url":"https:\/\/stream.radioangrezi.de:8000\/live-low",
+            "url":"https:\/\/stream.radioangrezi.de\/live-low",
             "codec":"mp3",
             "bitrate":64,
             "mobile":"1"
